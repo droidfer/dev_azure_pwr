a4aab88b6e07271a8139e6fb49fb134f4d355e26
Fernando <fmartinez@dotech.com.py>
2023-08-24 17:50:09 -0400

    WinRM commands 2

--- VMstart.ps1
+++ VMstart.ps1
@@ -1,10 +1,21 @@
-winrm quickconfig -Force
+
 
 $myIP = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
+Write-Output "Configure IP" $myIp
+
 $Cert = New-SelfSignedCertificate -DnsName $myIP -CertStoreLocation Cert:\LocalMachine\My
 $thumb = $Cert.Thumbprint
+Write-Output "Certification" $thumb 
+
+
 $params = '@{Hostname="' + $myIp + '"; CertificateThumbprint="' + $thumb + '"}'
+Write-Output "WinRM Params" $params 
+
 winrm create winrm/config/Listener?Address=*+Transport=HTTPS $params
+
+
+winrm quickconfig -Force
+
 
 set-item WSMan:\localhost\Client\TrustedHosts "*" -Force
 Enable-PSRemoting -Force

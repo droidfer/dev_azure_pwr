7daeab56eedd7ea13145871ff6512b793ef58f0e
Fernando <fmartinez@dotech.com.py>
2023-08-25 12:02:12 -0400

    Next

--- VMstart.ps1
+++ VMstart.ps1
@@ -1,22 +1,33 @@
-$myIPcomplex = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
-$myIP = $myIPcomplex.ToString()
-Write-Output "Configure IP" $myIp
-
-$Cert = New-SelfSignedCertificate -DnsName $myIP -CertStoreLocation Cert:\LocalMachine\My
-$thumb = $Cert.Thumbprint.ToString()
-Write-Output "Certification" $thumb 
-
-
-$params = '@{Hostname="' + $myIp + '"; CertificateThumbprint="' + $thumb + '"}'
-Write-Output "WinRM Params" $params 
-
-winrm create winrm/config/Listener?Address=*+Transport=HTTPS $params
-
-
-winrm quickconfig -Force
-
-
-set-item WSMan:\localhost\Client\TrustedHosts "*" -Force
 Enable-PSRemoting -Force
 
-Set-NetFirewallProfile -All -Enabled False
+New-NetFirewallRule -Name "Allow WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
+
+$setip = "myvm.eastus2.cloudapp.azure.com"
+
+$thumbprint = (New-SelfSignedCertificate -DnsName $setip -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+
+$command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$setip""; CertificateThumbprint=""$thumbprint""}"
+cmd.exe /C $command
+
+# $myIPcomplex = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
+# $myIP = $myIPcomplex.ToString()
+# Write-Output "Configure IP" $myIp
+
+# $Cert = New-SelfSignedCertificate -DnsName $myIP -CertStoreLocation Cert:\LocalMachine\My
+# $thumb = $Cert.Thumbprint.ToString()
+# Write-Output "Certification" $thumb 
+
+
+# $params = '@{Hostname="' + $myIp + '"; CertificateThumbprint="' + $thumb + '"}'
+# Write-Output "WinRM Params" $params 
+
+# winrm create winrm/config/Listener?Address=*+Transport=HTTPS $params
+
+
+# winrm quickconfig -Force
+
+
+# set-item WSMan:\localhost\Client\TrustedHosts "*" -Force
+# Enable-PSRemoting -Force
+
+# Set-NetFirewallProfile -All -Enabled False

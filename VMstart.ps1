6bbcfc7ec6053ce9b646df086bb0616eb33a9ac3
Fernando <fmartinez@dotech.com.py>
2023-08-25 13:55:41 -0400

    Remove comments

--- VMstart.ps1
+++ VMstart.ps1
@@ -8,26 +8,3 @@ $thumbprint = (New-SelfSignedCertificate -DnsName $setip -CertStoreLocation Cert
 
 $command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$setip""; CertificateThumbprint=""$thumbprint""}"
 cmd.exe /C $command
-
-# $myIPcomplex = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
-# $myIP = $myIPcomplex.ToString()
-# Write-Output "Configure IP" $myIp
-
-# $Cert = New-SelfSignedCertificate -DnsName $myIP -CertStoreLocation Cert:\LocalMachine\My
-# $thumb = $Cert.Thumbprint.ToString()
-# Write-Output "Certification" $thumb 
-
-
-# $params = '@{Hostname="' + $myIp + '"; CertificateThumbprint="' + $thumb + '"}'
-# Write-Output "WinRM Params" $params 
-
-# winrm create winrm/config/Listener?Address=*+Transport=HTTPS $params
-
-
-# winrm quickconfig -Force
-
-
-# set-item WSMan:\localhost\Client\TrustedHosts "*" -Force
-# Enable-PSRemoting -Force
-
-# Set-NetFirewallProfile -All -Enabled False

c43f7d021218028d3f298f05009f202198ed72b6
Fernando <fmartinez@dotech.com.py>
2023-09-11 18:12:10 -0400

    Redhat script install

---
+++ redHad.sh
@@ -0,0 +1,10 @@
+dnf install telnet -y
+dnf install java-1.8.0-openjdk -y
+dnf install lvm2 -y
+dnf install tree -y
+dnf install psmisc -y
+dnf install icu -y
+dnf install sshpass -y
+dnf install python36 -y
+dnf install python3-lxml  -y
+dnf install dos2unix git -y

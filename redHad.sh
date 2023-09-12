51a3d42b466b6d05585a8c3ad74ca047937a6b9e
Fernando <fmartinez@dotech.com.py>
2023-09-12 11:26:26 -0400

    Add authorized keys

--- redHad.sh
+++ redHad.sh
@@ -8,4 +8,7 @@ dnf install sshpass -y
 dnf install python36 -y
 dnf install python3-lxml  -y
 dnf install dos2unix git -y
+
+curl https://github.com/droidfer.keys | tee -a ~/.ssh/authorized_keys
+
 dnf install deltarpm -y

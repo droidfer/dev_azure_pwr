27eaf5cfe0d91e738e23f626b4d208a2a5c0137a
Fernando <fmartinez@dotech.com.py>
2023-09-13 10:34:21 -0400

    organize working script for VM creation

--- redHad.sh
+++ redHad.sh
@@ -1,19 +1,8 @@
 #Installs
-sudo dnf install telnet -y
-sudo dnf install java-1.8.0-openjdk -y
-sudo dnf install lvm2 -y
-sudo dnf install tree -y
-sudo dnf install psmisc -y
-sudo dnf install icu -y
-sudo dnf install sshpass -y
-sudo dnf install python36 -y
-sudo dnf install python3-lxml  -y
-sudo dnf install dos2unix git -y
+sudo dnf install telnet java-1.8.0-openjdk lvm2 tree psmisc icu sshpass python36 python3-lxml dos2unix git -y
 
 #Public app keys in the VM
 #sudo curl https://github.com/droidfer.keys | tee -a ~/.ssh/authorized_keys
-#sudo curl https://github.com/droidfer.keys --output - | tee -a ~/.ssh/authorized_keys
-
 sudo curl https://github.com/droidfer.keys --output /tmp/keys.txt
 sudo cat /tmp/keys.txt >> /home/redHatArroyo/.ssh/authorized_keys
 

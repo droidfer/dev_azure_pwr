09cb197d3d765b031f3b63a43106ee7f1231ee92
Fernando <fmartinez@dotech.com.py>
2023-09-13 10:42:27 -0400

    Simplify authorization

--- redHad.sh
+++ redHad.sh
@@ -2,7 +2,4 @@
 sudo dnf install telnet java-1.8.0-openjdk lvm2 tree psmisc icu sshpass python36 python3-lxml dos2unix git -y
 
 #Public app keys in the VM
-#sudo curl https://github.com/droidfer.keys | tee -a ~/.ssh/authorized_keys
-sudo curl https://github.com/droidfer.keys --output /tmp/keys.txt
-sudo cat /tmp/keys.txt >> /home/redHatArroyo/.ssh/authorized_keys
-
+sudo curl https://github.com/droidfer.keys | tee -a /home/redHatArroyo/.ssh/authorized_keys

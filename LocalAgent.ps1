a5c9d27beccb5c9da53963de206ae09651d6f4ae
Fernando <fmartinez@dotech.com.py>
2023-09-21 15:32:59 -0400

    Script adjustments

--- LocalAgent.ps1
+++ LocalAgent.ps1
@@ -1,21 +1,23 @@
-param (
-    [string]$token = 'not_token',
-    [string]$poolSet = 'FkPool',
-    [string]$agentName = 'vmWin'
+Param (
+    [string]$token,
+    [string]$poolSet,
+    [string]$agentName
 )
 
-Set-Location c: ; mkdir dwn
+# Crear archivo de trabajo
+New-Item -ItemType Directory c:\agent
 
+# Descargar Archivo a instalar
 $URL="https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-win-x64-3.225.0.zip"
-
-$Path=“C:\dwn\vsts-agent-win-x64-3.225.0.zip"
-
+$Path=“c:\agent\vsts-agent-win-x64-3.225.0.zip"
 (New-Object System.Net.WebClient).DownloadFile($URL, $Path)
 
-Set-Location c: ;  mkdir agent ; Set-Location agent
+# Extraer archivo de zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem 
+[System.IO.Compression.ZipFile]::ExtractToDirectory($Path, "c:\agent\")
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory($Path, "C:\agent\")
-
+# Ejecutar comando
 .\config.cmd --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token $token --pool $poolSet --agent $agentName --work _work --runAsService N --runAsAutoLogon N
 
+# Iniciar
 .\run.cmd

cbd5aa61b4a304951b5d8bc26f365b4bd99fb31d
Fernando <fmartinez@dotech.com.py>
2023-09-25 10:27:04 -0400

    Redhat LocalAgent script 3

--- redHadAgent.sh
+++ redHadAgent.sh
@@ -3,11 +3,11 @@ sudo curl https://github.com/droidfer.keys | tee -a /home/redHatArroyo/.ssh/auth
 
 # Donwload and Install VTST Agent
 mkdir /home/redHatArroyo/myagent
-sudo curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
+curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
 tar zxvf /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz -C /home/redHatArroyo/myagent/
 
 # Configure Agent
-/home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token $1 --pool $2 --agent $3
+/home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token $0 --pool $1 --agent $2
 
 # Start Agent
 /home/redHatArroyo/myagent/run.sh

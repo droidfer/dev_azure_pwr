9d8a4e6b10cb523471635f46b4c4c0ae9dfe036f
Fernando <fmartinez@dotech.com.py>
2023-09-25 10:52:36 -0400

    remove sudo

--- redHadAgent.sh
+++ redHadAgent.sh
@@ -2,12 +2,12 @@
 sudo curl https://github.com/droidfer.keys | tee -a /home/redHatArroyo/.ssh/authorized_keys
 
 # Donwload and Install VTST Agent
-sudo mkdir /home/redHatArroyo/myagent
-sudo curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
-sudo tar zxvf /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz -C /home/redHatArroyo/myagent/
+mkdir /home/redHatArroyo/myagent
+curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
+tar zxvf /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz -C /home/redHatArroyo/myagent/
 
 # Configure Agent
-sudo /home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token $0 --pool $1 --agent $2
+/home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token $0 --pool $1 --agent $2
 
 # Start Agent
-sudo /home/redHatArroyo/myagent/run.sh
+/home/redHatArroyo/myagent/run.sh

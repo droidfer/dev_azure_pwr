555fadfb78a59ba10c82eec9fb40f6193c695cb9
Fernando <fmartinez@dotech.com.py>
2023-09-25 13:28:46 -0400

    more

--- redHadAgent.sh
+++ redHadAgent.sh
@@ -2,15 +2,15 @@
 sudo curl https://github.com/droidfer.keys | tee -a /home/redHatArroyo/.ssh/authorized_keys
 
 # Change User
-su redHatArroyo
+#su redHatArroyo
 
 # Donwload and Install VTST Agent
-mkdir /home/redHatArroyo/myagent
-curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
-tar zxvf /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz -C /home/redHatArroyo/myagent/
+#mkdir /home/redHatArroyo/myagent
+#curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
+#tar zxvf /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz -C /home/redHatArroyo/myagent/
 
 # Configure Agent
-/home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token zt2fj23opwnne6gvdbzoiru7lriz3wzqfgmpmm7ib2ivfsnfndiq --pool FkPool --agent redhatPool
+#/home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token zt2fj23opwnne6gvdbzoiru7lriz3wzqfgmpmm7ib2ivfsnfndiq --pool FkPool --agent redhatPool
 
 # Start Agent
-/home/redHatArroyo/myagent/run.sh
+#/home/redHatArroyo/myagent/run.sh

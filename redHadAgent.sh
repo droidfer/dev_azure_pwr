4f59c3a72012c100811e9fcb9330f8ff9e3326ea
Fernando <fmartinez@dotech.com.py>
2023-09-26 09:25:04 -0400

    Minima configuration

--- redHadAgent.sh
+++ redHadAgent.sh
@@ -6,15 +6,3 @@ su redHatArroyo
 
 # Download and Install VTST Agent
 mkdir /home/redHatArroyo/myagent
-curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz
-
-tar zxvf /home/redHatArroyo/myagent/vsts-agent-linux-x64-3.225.0.tar.gz -C /home/redHatArroyo/myagent/
-
-# Allow all permisions to redHatArroyo user
-sudo chown -R redHatArroyo /home/redHatArroyo/myagent/
-
-# Configure Agent
-su redHatArroyo -c /home/redHatArroyo/myagent/config.sh --unattended --url https://dev.azure.com/ArroyoTraining/ --auth pat --token $1 --pool $2 --agent $3
-
-# Start Agent
-su redHatArroyo -c /home/redHatArroyo/myagent/run.sh

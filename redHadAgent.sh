033b1e40b148182fb0cc926e27a6a04938cb0b9f
Fernando <fmartinez@dotech.com.py>
2023-09-26 09:26:21 -0400

    Min conf

--- redHadAgent.sh
+++ redHadAgent.sh
@@ -5,4 +5,5 @@ sudo curl https://github.com/droidfer.keys | tee -a /home/redHatArroyo/.ssh/auth
 su redHatArroyo
 
 # Download and Install VTST Agent
-mkdir /home/redHatArroyo/myagent
+mkdir /home/redHatArroyo/dwn
+curl https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz > /home/redHatArroyo/dwn/vsts-agent-linux-x64-3.225.0.tar.gz
